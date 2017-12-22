a5c3010f832d0bb2f5f10e70499c10666abffa1f
Thomas Arts <thomas.arts@quviq.com>
2017-12-22 13:44:26 +0100

    Do not get mempool from other chains

--- apps/aecore/src/aec_sync.erl
+++ apps/aecore/src/aec_sync.erl
@@ -82,32 +82,33 @@ compare_ping_objects(Local, Remote) ->
     Src = maps:get(<<"source">>, Remote),
     case {maps:get(<<"genesis_hash">>, Local),
           maps:get(<<"genesis_hash">>, Remote)} of
-        {G, G} ->
-            lager:debug("genesis blocks match", []),
-            %% same genesis block - continue
-            case {maps:get(<<"best_hash">>, Local),
-                  maps:get(<<"best_hash">>, Remote)} of
-                {T, T} ->
-                    lager:debug("same top blocks", []),
-                    %% headers in sync; check missing blocks
-                    %% Note that in this case, both will publish
-                    %% events as if they're the server (basically
-                    %% meaning that they were tied for server position).
-                    server_get_missing_blocks(Src);
-                _ ->
-                    Dl = maps:get(<<"difficulty">>, Local),
-                    Dr = maps:get(<<"difficulty">>, Remote),
-                    if Dl > Dr ->
-                        lager:debug("Our difficulty is higher", []),
-                        server_get_missing_blocks(Src);
-                       true ->
-                         start_sync(Src)
-                    end
-             end,
-             fetch_mempool(Src),
-             ok;
-        _ ->
-             {error, different_genesis_blocks}
+      {G, G} ->
+        lager:debug("genesis blocks match", []),
+        %% same genesis block - continue
+        case {maps:get(<<"best_hash">>, Local),
+              maps:get(<<"best_hash">>, Remote)} of
+          {T, T} ->
+            lager:debug("same top blocks", []),
+            %% headers in sync; check missing blocks
+            %% Note that in this case, both will publish
+            %% events as if they're the server (basically
+            %% meaning that they were tied for server position).
+            server_get_missing_blocks(Src);
+          _ ->
+            Dl = maps:get(<<"difficulty">>, Local),
+            Dr = maps:get(<<"difficulty">>, Remote),
+            if Dl > Dr ->
+                lager:debug("Our difficulty is higher", []),
+                server_get_missing_blocks(Src),
+                ok;
+               true ->
+                start_sync(Src)
+            end
+        end,
+        fetch_mempool(Src),
+        ok;
+      _ ->
+        {error, different_genesis_blocks}
     end.
 
 start_sync(PeerUri) ->
